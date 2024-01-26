b3f5da53533868f970bde544c6296c2a9ca7894d
Robert Szałapak <robsza1212@gmail.com>
2024-01-26 17:49:26 +0100

    make build

---
+++ index.135f9600.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=e.clientHeight,l=e.clientWidth,i=t.clientHeight,n=t.clientWidth;e.style.top="".concat(i/2-c/2,"px"),e.style.left="".concat(n/2-l/2,"px");
+//# sourceMappingURL=index.135f9600.js.map
